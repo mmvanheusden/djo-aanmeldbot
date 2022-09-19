06eac0868c2d326e30144421c355f11011ae0e32
mmvanheusden <50550545+mmvanheusden@users.noreply.github.com>
2022-09-19 18:09:35 +0200

    Add persistent storage + afmelden

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ Please set the 'telegram_token' as an environment variable
 import logging
 import os
 
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes, PicklePersistence
+
 from idp_renew.auth import *
 from idp_renew.register import Register
-
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CallbackQueryHandler, CommandHandler, ContextTypes
 
 # Enable logging
 
@@ -27,19 +27,35 @@ logger = logging.getLogger(__name__)
 async def login(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Explains how to use the bot."""
 
-    if len(context.args) != 2:
-        await update.message.reply_text("Gebruik /login <email> <wachtwoord> om in te loggen")
+    # Check if the user_data is empty
+    if update.effective_chat.id not in context.user_data:
+        if len(context.args) != 2:
+            await update.message.reply_text("Gebruik /login <email> <wachtwoord> om in te loggen")
+        else:
+            # Get the username and password from the message
+            username, password = update.message.text.split(" ")[1:]
+
+            # Save the username and password in the user_data
+            context.user_data[update.effective_chat.id] = [username, password]
+
+            await update.message.reply_text("Je bent ingelogd! /aanmelden om je aan te melden")
     else:
-        # Get the username and password from the message
-        username, password = update.message.text.split(" ")[1:]
-
-        # Save the username and password in the user_data
-        context.user_data[update.effective_chat.id] = [username, password]
-
-        await update.message.reply_text("Je bent ingelogd!")
+        await update.message.reply_text("Je bent al ingelogd! log uit met /logout")
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def logout(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Logs the user out."""
+
+    # Check if the user is logged in
+    if update.effective_chat.id not in context.user_data:
+        await update.message.reply_text("Je bent niet ingelogd! gebruik /login <email> <wachtwoord> om in te loggen.")
+        return
+    else:
+        del context.user_data[update.effective_chat.id]
+        await update.message.reply_text("Je bent uitgelogd!")
+
+
+async def aanmelden(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Sends a message with the date buttons."""
 
     # Check if the user is logged in
@@ -49,14 +65,34 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         keyboard = [
             [
-                InlineKeyboardButton("Vrijdag", callback_data="vrijdag"),
-                InlineKeyboardButton("Zaterdag", callback_data="zaterdag"),
+                InlineKeyboardButton("Vrijdag", callback_data="vrijdagaanmelden"),
+                InlineKeyboardButton("Zaterdag", callback_data="zaterdagaanmelden"),
             ],
         ]
 
         reply_markup = InlineKeyboardMarkup(keyboard)
 
         await update.message.reply_text("Meld je aan", reply_markup=reply_markup)
+
+
+async def afmelden(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Sends a message with the date buttons."""
+
+    # Check if the user is logged in
+    if update.effective_chat.id not in context.user_data:
+        await update.message.reply_text("Je bent niet ingelogd! gebruik /login <email> <wachtwoord> om in te loggen.")
+        return
+    else:
+        keyboard = [
+            [
+                InlineKeyboardButton("Vrijdag", callback_data="vrijdagafmelden"),
+                InlineKeyboardButton("Zaterdag", callback_data="zaterdagafmelden"),
+            ],
+        ]
+
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        await update.message.reply_text("Meld je af", reply_markup=reply_markup)
 
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -72,7 +108,8 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
         server.authenticate()
     except Exception as e:
-        await query.edit_message_text("Je gebruikersnaam of wachtwoord is niet correct! pas het aan met /login")
+        await query.edit_message_text(
+            "Je gebruikersnaam of wachtwoord is niet correct! pas het aan met /login <email> <wachtwoord>")
         # await query.message.reply_markdown(f" Debug:\n`{str(e)}`")
         return
 
@@ -80,41 +117,54 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     for pod in pods:
         if not pod.available or pod.closed:
-            print("Niet beschikbaar of gesloten")
-            continue
-
-        if pod.available == 0:
+            if pod.pod == "e" and query.data == "vrijdagaanmelden":
+                await query.edit_message_text(f"Het is niet mogelijk om je aan te melden voor Vrijdag")
+                return
+            elif pod.pod == "f" and query.data == "zaterdagaanmelden":
+                await query.edit_message_text(f"Het is niet mogelijk om je aan te melden voor Zaterdag")
+                return
             print("Niet beschikbaar")
             continue
 
-        if query.data == "vrijdag":
+        if "vrijdag" in query.data:
             if pod.pod == "e":
-                # try to register
                 try:
-                    pod.register()
-                    print("Aangemeld voor Vrijdag")
+                    if "aanmelden" in query.data:
+                        pod.register()
+                        print("Aangemeld voor Vrijdag")
+                        await query.edit_message_text("Je bent aangemeld voor Vrijdag! /afmelden om je af te melden")
+                    else:
+                        pod.deregister()
+                        print("Afgemeld voor Vrijdag")
+                        await query.edit_message_text("Je bent afgemeld voor Vrijdag! /aanmelden om je aan te melden")
                 except:
-                    print("Error bij aanmelden voor Vrijdag")
+                    print("Error bij aanmelden/afmelden voor Vrijdag")
         else:
             if pod.pod == "m":
-                # try to register
                 try:
-                    pod.register()
-                    print("Aangemeld voor Zaterdag")
+                    if "aanmelden" in query.data:
+                        pod.register()
+                        print("Aangemeld voor Zaterdag")
+                        await query.edit_message_text("Je bent aangemeld voor Zaterdag! /afmelden om je af te melden")
+                    else:
+                        pod.deregister()
+                        print("Afgemeld voor Zaterdag")
+                        await query.edit_message_text("Je bent afgemeld voor Zaterdag! /aanmelden om je aan te melden")
                 except:
-                    print("Error bij aanmelden voor Zaterdag")
+                    print("Error bij aanmelden/afmelden voor Zaterdag")
 
     await query.answer()
-    await query.edit_message_text(text=f"Je bent aangemeld voor {query.data.capitalize()}.")
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Displays info on how to use the bot."""
 
     await update.message.reply_markdown_v2("""
     *__DJO Aanmelden__*
+*Gebruik /aanmelden om aan te melden*
+*Gebruik /afmelden om af te melden*
 *Gebruik /login \<email\> \<wachtwoord\> om in te loggen*
-*Gebruik /start om je aan te melden*
+*Gebruik /logout om uit te loggen*
     """)
 
 
@@ -123,15 +173,16 @@ def main() -> None:
 
     # Create the Application and pass it your bot's token.
 
-    application = Application.builder().token(os.getenv("telegram_token")).build()
+    persistence = PicklePersistence(filepath="aanmeldbot")
+    application = Application.builder().token(os.getenv("telegram_token")).persistence(persistence).build()
 
-    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler(("start", "help"), start))
+    application.add_handler(CommandHandler("aanmelden", aanmelden))
+    application.add_handler(CommandHandler("afmelden", afmelden))
+    application.add_handler(CommandHandler("login", login))
+    application.add_handler(CommandHandler("logout", logout))
 
     application.add_handler(CallbackQueryHandler(button))
-
-    application.add_handler(CommandHandler("help", help_command))
-
-    application.add_handler(CommandHandler("login", login))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
